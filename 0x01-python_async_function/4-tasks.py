4179aa7f8ca87b03168ce436c9e38068c4508cc8
Jong9106 <juank_hr@hotmail.com>
2022-06-07 23:14:03 -0500

    FIX | Range list

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-"""module"""
+""" Takes int arg, waits for random delay """
 
 from typing import List
-
-
+import asyncio
+import random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """spawn wait_random n times with the specified max_delay"""
-    lst = []
-
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Waits for ran delay until max_delay, returns list of actual delays """
+    spawn_list = []
+    delay_list = []
     for i in range(n):
-        lst.append(await task_wait_random(max_delay))
-    return sorted(lst)
+        delayed_task = task_wait_random(max_delay)
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(delayed_task)
+
+    for spawn in spawn_list:
+        await spawn
+
+    return delay_list
