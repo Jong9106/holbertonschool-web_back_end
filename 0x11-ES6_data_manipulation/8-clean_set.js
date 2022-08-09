af2f70b75dc39f5ef5052b97496f319ea713e153
Jong9106 <juank_hr@hotmail.com>
2022-08-09 18:07:53 -0500

    ADD | New file

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
-export default function cleanSet(set, startString) {
-  const string = [];
-
-  if (typeof startString !== 'string' || typeof set !== 'object' || startString.length === 0) {
-    return '';
+export default function cleanSet(_set, startString) {
+	if (typeof _set !== 'object') return '';
+	if (typeof startString !== 'string') return '';
+	if (startString.length === 0) return '';
+  
+	const stringSet = [];
+	[..._set].forEach((x) => {
+	  if (x && x.indexOf(startString) === 0) stringSet.push(x.replace(startString, ''));
+	});
+	return stringSet.join('-');
   }
-  for (const item of set) {
-    string.push(item.slice(startString.length));
-  }
-  return string.join('-');
-}
